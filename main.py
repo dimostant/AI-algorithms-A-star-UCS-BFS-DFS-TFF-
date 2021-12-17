9b419df2e90a7f9982a2c15042fcecdfe60fab02
dimostant <dimarasgv@gmail.com>
2021-12-18 00:19:23 +0200

    graph is now in order

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     standard_cost = 1
     door_cost = 2
     print("============ UCS Search ================")
-    path_ucs, explored_ucs = uniform_cost_search(graph_neighbours, '34', '92', standard_cost, door_cost)
+    path_ucs, explored_ucs = uniform_cost_search(graph_neighbours, '55', '92', standard_cost, door_cost)
     #path_ucs = ucs(graph_neighbours, '0', '92')
     print("Path UCS:", path_ucs)
     #print("Explored Nodes UCS: ", explored_ucs)
--- originalGraph.py
+++ originalGraph.py
@@ -61,8 +61,8 @@ def generate_graph():
     add_neighbours('47', ['40', '48'])
 
 
-    #add_neighbours('48', ['47', '41', '55D'])
-    add_neighbours('48', ['41', '47', '55'])
+    add_neighbours('48', ['47', '41', '55D'])
+    #add_neighbours('48', ['41', '47', '55'])
     add_neighbours('49', ['50'])
     add_neighbours('50', ['49', '51'])
     add_neighbours('51', ['42','50','58'])
@@ -73,8 +73,10 @@ def generate_graph():
     add_neighbours('54', ['46', '62'])  
 
 
-    #add_neighbours('56', ['55D', '57']) 
+    
     add_neighbours('55', ['48', '56', '65'])
+    add_neighbours('56', ['55D', '57'])
+    #add_neighbours('56', ['55', '57'])  
     add_neighbours('56', ['55', '57']) 
     add_neighbours('57', ['56', '66'])
     add_neighbours('58', ['51', '69'])
@@ -86,8 +88,8 @@ def generate_graph():
     add_neighbours('63', ['62', '64'])
     add_neighbours('64', ['63', '65'])
 
-    #add_neighbours('65', ['64', '55D', '75'])
-    add_neighbours('65', ['55', '64', '75'])
+    add_neighbours('65', ['64', '55D', '75'])
+    #add_neighbours('65', ['55', '64', '75'])
     add_neighbours('66', ['57', '67'])
     add_neighbours('67', ['66', '68'])
     add_neighbours('68', ['67', '69'])
@@ -99,8 +101,8 @@ def generate_graph():
     add_neighbours('73', ['61', '72', '74', '78'])
     add_neighbours('74', ['62', '73', '79'])
     add_neighbours('75', ['65', '81'])
-    #add_neighbours('76', ['69', '86D'])
-    add_neighbours('76', ['69', '86'])
+    add_neighbours('76', ['69', '86D'])
+    #add_neighbours('76', ['69', '86'])
 
     add_neighbours('77', ['72', '78', '87'])
     add_neighbours('78', ['73', '77', '79'])
@@ -111,17 +113,16 @@ def generate_graph():
     add_neighbours('83', ['82', '84'])
     add_neighbours('84', ['83', '85'])
 
-    #add_neighbours('85', ['84', '91', '86D'])
-    add_neighbours('85', ['84', '86', '91'])
+    add_neighbours('85', ['84', '91', '86D'])
+    #add_neighbours('85', ['84', '86', '91'])
     add_neighbours('86', ['76', '85','92'])
     add_neighbours('87', ['77'])
     add_neighbours('88', ['80', '89'])
     add_neighbours('89', ['88', '90'])
     add_neighbours('90', ['81', '89'])
     add_neighbours('91', ['85', '92'])
-    #add_neighbours('92', ['91', '86D'])
-
-    add_neighbours('92', ['86', '91'])
+    add_neighbours('92', ['91', '86D'])
+    #add_neighbours('92', ['86', '91'])
 
     return graph_neighbours
 
